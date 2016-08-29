3c27320c2db65e8461e513d8853d11d8909c992c
Yoorian Shin <yoorian.shin@gmail.com>
2016-08-29 16:49:18 -0700

    Initial Commit

---
+++ TouchTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchTracker
+//
+//  Created by Yoorian YeEun Shin on 8/29/16.
+//  Copyright © 2016 Yoorian Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
