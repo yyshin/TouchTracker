364d080e889d9f02177899aed240e89fc25f75e8
Yoorian Shin <yoorian.shin@gmail.com>
2016-08-29 16:54:00 -0700

    Touch events and UIResponder added

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,16 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Yoorian YeEun Shin on 8/29/16.
+//  Copyright © 2016 Yoorian Shin. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
